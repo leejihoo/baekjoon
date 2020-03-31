b269d43b503408c4d879c2a3711151700058a89b
mint97 <ajtwlsdl33@gmail.com>
2020-03-31 20:14:53 +0900

    백준 11654 아스키 코드 - 서식문자

---
+++ baekjoon/11654.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#pragma warning(disable : 4996)
+using namespace std;
+
+int main(void)
+{
+	char result;
+	scanf("%c", &result);
+	if(47<result && result <58)
+	printf("%d", result);
+	else
+	{
+		printf("%d", result);
+	}
+}
